fc199591951f6e3eeca4cc76392b5cf8cf1b54bc
J017_김기한 <jum654@naver.com>
2021-09-27 18:32:17 +0900

    feat: 프로젝트 구조 생성
    - webpack bolierplate를 이용하여 webpack-express 프로젝트 생성

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************!*\
+  !*** ./public/js/index.js ***!
+  \****************************/
+document.querySelector('#test').innerText = 'abcdefg';
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoianMvbWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBQSxRQUFRLENBQUNDLGFBQVQsQ0FBdUIsT0FBdkIsRUFBZ0NDLFNBQWhDLEdBQTRDLFNBQTVDLEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9qYXZhc2NyaXB0LXAyLWFpcmJuYi8uL3B1YmxpYy9qcy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcjdGVzdCcpLmlubmVyVGV4dCA9ICdhYmNkZWZnJyJdLCJuYW1lcyI6WyJkb2N1bWVudCIsInF1ZXJ5U2VsZWN0b3IiLCJpbm5lclRleHQiXSwic291cmNlUm9vdCI6IiJ9
