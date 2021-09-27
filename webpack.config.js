1083bd0e9316839dd5ba3d10e247cdcc3862a487
J017_김기한 <jum654@naver.com>
2021-09-27 18:32:17 +0900

    feat: 프로젝트 구조 생성
    - webpack bolierplate를 이용하여 webpack-express 프로젝트 생성

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        main: "./public/js/index.js",
+        reset: "./public/css/reset.scss",
+    },
+    output: {
+        path: path.resolve(__dirname, "assets"),
+        filename: "js/[name].js",
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: "css/[name].css" })],
+    devServer: {
+        contentBase: path.resolve(__dirname, "src"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(svg|gif|otf)$/,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: "url-loader",
+                options: {
+                    name: "images/[name].txt",
+                },
+            },
+        ],
+    },
+    devtool: "inline-source-map",
+    mode: "development",
+};
