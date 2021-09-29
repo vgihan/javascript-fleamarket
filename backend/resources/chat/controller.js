b968772bf50f5299e42892050b9bb7e241e7090c
J017_김기한 <jum654@naver.com>
2021-09-29 23:23:35 +0900

    fix: 리소스 별 controller 로직에 try/catch 추가
    - 데이터베이스에서 발생한 에러 처리를 위해 controller 로직에 try/catch
      추가

--- backend/resources/chat/controller.js
+++ backend/resources/chat/controller.js
@@ -4,20 +4,31 @@ const service = new Service(models);
 const { requestValidation } = require("../requestValidation");
 
 async function get(req, res) {
-    const validCons = ["item_iid", "sender", "recver"];
-    const validation = requestValidation(
-        [
-            Object.keys(req.query).filter((param) => !validCons.includes(param))
-                .length <= 0,
-        ],
-        res
-    );
-    if (!validation) return;
-    res.json(await service.findChat(req.query));
+    try {
+        const validCons = ["item_iid", "sender", "recver"];
+        const validation = requestValidation(
+            [
+                Object.keys(req.query).filter(
+                    (param) => !validCons.includes(param)
+                ).length <= 0,
+            ],
+            res
+        );
+        if (!validation) return;
+        res.json(await service.findChat(req.query));
+    } catch (error) {
+        res.status(400).json({ message: "Bad Request" });
+    }
 }
 async function post(req, res) {
-    const { item_iid, sender, recver, contents } = req.body;
-    res.json(await service.registChat({ item_iid, sender, recver, contents }));
+    try {
+        const { item_iid, sender, recver, contents } = req.body;
+        res.json(
+            await service.registChat({ item_iid, sender, recver, contents })
+        );
+    } catch (error) {
+        res.status(400).json({ message: "Bad Request" });
+    }
 }
 
 module.exports = { get, post };
--- backend/resources/wishlist/controller.js
+++ backend/resources/wishlist/controller.js
@@ -9,32 +9,42 @@ async function get(req, res) {
     res.json(await service.findWishlist(req.query));
 }
 async function post(req, res) {
-    const validCons = ["user_uid", "item_iid"];
-    const validation = requestValidation(
-        [
-            Object.keys(req.body).filter((param) => !validCons.includes(param))
-                .length <= 0,
-            req.body.user_uid,
-            req.body.item_iid,
-        ],
-        res
-    );
-    if (!validation) return;
-    res.json(await service.registWishlist(req.body));
+    try {
+        const validCons = ["user_uid", "item_iid"];
+        const validation = requestValidation(
+            [
+                Object.keys(req.body).filter(
+                    (param) => !validCons.includes(param)
+                ).length <= 0,
+                req.body.user_uid,
+                req.body.item_iid,
+            ],
+            res
+        );
+        if (!validation) return;
+        res.json(await service.registWishlist(req.body));
+    } catch (err) {
+        res.status(400).json({ message: 400 });
+    }
 }
 async function del(req, res) {
-    const validCons = ["user_uid", "item_iid"];
-    const validation = requestValidation(
-        [
-            Object.keys(req.query).filter((param) => !validCons.includes(param))
-                .length <= 0,
-            req.query.user_uid,
-            req.query.item_iid,
-        ],
-        res
-    );
-    if (!validation) return;
-    res.json(await service.deleteWishlist(req.query));
+    try {
+        const validCons = ["user_uid", "item_iid"];
+        const validation = requestValidation(
+            [
+                Object.keys(req.query).filter(
+                    (param) => !validCons.includes(param)
+                ).length <= 0,
+                req.query.user_uid,
+                req.query.item_iid,
+            ],
+            res
+        );
+        if (!validation) return;
+        res.json(await service.deleteWishlist(req.query));
+    } catch (error) {
+        res.status(400).json({ message: 400 });
+    }
 }
 
 module.exports = { get, post, del };
