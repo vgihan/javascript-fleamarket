4b3c5366b7fc6317ce67a7469dbf305809d2137a
J017_김기한 <jum654@naver.com>
2021-09-29 23:23:35 +0900

    fix: 리소스 별 controller 로직에 try/catch 추가
    - 데이터베이스에서 발생한 에러 처리를 위해 controller 로직에 try/catch
      추가

--- backend/resources/item/controller.js
+++ backend/resources/item/controller.js
@@ -24,22 +24,33 @@ async function get(req, res) {
     res.json(await service.findItem(req.query));
 }
 async function post(req, res) {
-    const validCons = ["user_uid", "title", "price", "contents", "category"];
-    const { user_uid, title, price, contents, category } = req.body;
-    const validation = requestValidation(
-        [
-            Object.keys(req.body).filter((param) => !validCons.includes(param))
-                .length <= 0,
-            user_uid.length < 20,
-            title.length < 25,
-            price < Number.MAX_VALUE,
-            contents.length < 50,
-            category.length < 8,
-        ],
-        res
-    );
-    if (!validation) return;
-    res.json(await service.registItem(req.body));
+    try {
+        const validCons = [
+            "user_uid",
+            "title",
+            "price",
+            "contents",
+            "category",
+        ];
+        const { user_uid, title, price, contents, category } = req.body;
+        const validation = requestValidation(
+            [
+                Object.keys(req.body).filter(
+                    (param) => !validCons.includes(param)
+                ).length <= 0,
+                user_uid.length < 20,
+                title.length < 25,
+                price < Number.MAX_VALUE,
+                contents.length < 50,
+                category.length < 8,
+            ],
+            res
+        );
+        if (!validation) return;
+        res.json(await service.registItem(req.body));
+    } catch (err) {
+        res.status(400).json({ message: "Bad request" });
+    }
 }
 async function put(req, res) {
     const validCons = [
--- backend/resources/user/controller.js
+++ backend/resources/user/controller.js
@@ -5,18 +5,23 @@ const { requestValidation } = require("../requestValidation");
 
 function get(req, res) {}
 async function post(req, res) {
-    const validCons = ["uid", "locate"];
-    const validation = requestValidation(
-        [
-            Object.keys(req.body).filter((param) => !validCons.includes(param))
-                .length <= 0,
-            req.body.uid,
-            req.body.locate,
-        ],
-        res
-    );
-    if (!validation) return;
-    res.json(await service.registUser(req.body));
+    try {
+        const validCons = ["uid", "locate"];
+        const validation = requestValidation(
+            [
+                Object.keys(req.body).filter(
+                    (param) => !validCons.includes(param)
+                ).length <= 0,
+                req.body.uid,
+                req.body.locate,
+            ],
+            res
+        );
+        if (!validation) return;
+        res.json(await service.registUser(req.body));
+    } catch (error) {
+        res.status(400).json({ message: 400 });
+    }
 }
 
 module.exports = { get, post };
