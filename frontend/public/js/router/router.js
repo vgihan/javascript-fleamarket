189e8c36a8114a5ecc1d8c34c809bea6cf80f30e
J017_김기한 <jum654@naver.com>
2021-09-30 19:32:25 +0900

    feat: 더미 데이터를 이용한 메인 페이지 contents 구성
    - store에 초기 값을 넣어두고 메인 페이지 contents를 구성함.
    - 폰트가 일정 크기 이하로 줄어들지 않는 문제가 존재함.

--- frontend/public/js/router/router.js
+++ frontend/public/js/router/router.js
@@ -1,1 +1,16 @@
-export const routeMap = {};
+const { Main } = require("../components/main/main");
+
+const routeMap = {
+    "/": Main,
+};
+
+export function initRouter() {
+    routing();
+    window.addEventListener("popstate", routing);
+}
+
+function routing() {
+    const path = window.location.pathname;
+    const $root = document.querySelector(".root");
+    new routeMap[path]($root, {});
+}
