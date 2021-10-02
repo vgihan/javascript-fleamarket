3be1e6371d6a95fea9c6b5d849cfd9fd77357ffb
J017_김기한 <jum654@naver.com>
2021-10-02 20:25:04 +0900

    style: 로그인 페이지 style 구성

---
+++ frontend/public/js/components/login/login.js
@@ -0,0 +1,21 @@
+import { Component } from "../../core/component";
+
+export class Login extends Component {
+    template() {
+        return `<div class="login_wrap">
+            <div class="header">
+                <img src="assets/img/left_empty_arrow.png" />
+                <p>로그인</p>
+            </div>
+            <div class="login_box">
+                <form action="/login" method="POST">
+                    <input type="text" name="user_id" placeholder="아이디를 입력하세요." />
+                    <input type="submit" value="로그인"/>
+                </form>
+            </div>
+            <div class="signup_box">
+                <a href="/signup">회원가입</a>
+            </div>
+        </div>`;
+    }
+}
