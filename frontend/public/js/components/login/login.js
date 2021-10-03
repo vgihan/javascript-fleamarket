9b5551b916f7c8643ffb36a2d3b378f2befe7587
J017_김기한 <jum654@naver.com>
2021-10-03 21:58:01 +0900

    feat: session을 이용한 기본 회원가입-로그인 구현
    - 로그인 시 사용자 입력 정보와 데이터베이스 정보를 비교하여 일치하는
      유저가 존재하면 세션의 user_id 값이 변경되도록 구현

--- frontend/public/js/components/login/login.js
+++ frontend/public/js/components/login/login.js
@@ -8,7 +8,7 @@ export class Login extends Component {
                 <p>로그인</p>
             </div>
             <div class="login_box">
-                <form action="/login" method="POST">
+                <form action="/auth/local/login" method="POST">
                     <input type="text" name="user_id" placeholder="아이디를 입력하세요." />
                     <input type="submit" value="로그인"/>
                 </form>
