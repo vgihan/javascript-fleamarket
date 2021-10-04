3eed4896ff40b8711444a642369b1d97b319a2e2
J017_김기한 <jum654@naver.com>
2021-10-04 10:46:18 +0900

    feat: 로그아웃 서비스 로직 구현
    - 로그아웃 시 세션 삭제

--- backend/resources/auth/local/controller.js
+++ backend/resources/auth/local/controller.js
@@ -1,7 +1,6 @@
 const AuthService = require("../auth_service");
 const models = require("../../../models");
 const service = new AuthService(models);
-const { requestValidation } = require("../../requestValidation");
 
 async function login(req, res) {
     try {
@@ -24,5 +23,15 @@ async function signup(req, res) {
         res.redirect("/signup-page");
     }
 }
+async function logout(req, res) {
+    try {
+        req.session.destroy((err) => {
+            if (err) console.log(err);
+            res.redirect("/");
+        });
+    } catch (error) {
+        res.redirect("/login-page");
+    }
+}
 
-module.exports = { login, signup };
+module.exports = { login, signup, logout };
