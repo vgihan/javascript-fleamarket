b0fbfacd06527915d43134219fd25999279490a3
J017_김기한 <jum654@naver.com>
2021-10-06 17:03:44 +0900

    feat: OAuth를 이용한 회원가입 구현
    - 로그인 시 데이터베이스에 일치하는 유저가 존재하지 않으면 회원가입
      페이지로 이동
    - github id 를 userId로 사용하여 회원가입 진행

--- backend/resources/auth/github/index.js
+++ backend/resources/auth/github/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { githubLogin, githubLoginCallback } = require("./controller");
+const { githubLogin, githubLoginCallback, signup } = require("./controller");
 
 router.get("", githubLogin);
 router.get("/callback", githubLoginCallback);
+router.get("/signup", signup);
 
 module.exports = router;
