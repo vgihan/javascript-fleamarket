c7f75618ca60bf37c60c8ab924f303787f06fab5
J017_김기한 <jum654@naver.com>
2021-10-06 19:30:44 +0900

    feat: mypage 구현
    - 현재 로그인 하고 있는 유저의 userId를 확인할 수 있는 mypage를 구현함.

---
+++ frontend/public/js/components/mypage/mypage.js
@@ -0,0 +1,40 @@
+import { Component } from "../../core/component";
+import { routing } from "../../router/router";
+import { store } from "../../store/store";
+
+export class Mypage extends Component {
+    template() {
+        const {
+            user: { userId },
+        } = store.getState();
+        return `<div class="mypage_wrap">
+            <div class="header">
+                <a href="" class="back_btn">
+                    <img src="assets/img/left_empty_arrow.png" />
+                </a>
+                <p>내 계정</p>
+            </div>
+            <div class="mypage_contents">
+                <div class="contents_username">
+                    <p>${userId}</p>
+                </div>
+                <form action="/auth/github/logout" method="GET">
+                    <input type="submit" value="로그아웃"/>
+                </form>
+            </div>
+        </div>`;
+    }
+    setEvent() {
+        const signupBtn = document.querySelector(".signup_box > a");
+        const backBtn = document.querySelector(".header > a");
+
+        backBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            window.history.back();
+        });
+        signupBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            routing("/signup-page");
+        });
+    }
+}
