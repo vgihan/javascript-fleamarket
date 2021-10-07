8d476611197d54585e8ed9cd0effced07032d240
J017_김기한 <jum654@naver.com>
2021-10-07 16:02:28 +0900

    fix: 사진 첨부 기능을 위해 multer 미들웨어 추가 및 form 태그 구성
    - newpost contents 컴포넌트의 template에 form 태그를 추가함.
    - /item POST 시 multer 미들웨어를 통해 사진 저장.

--- frontend/public/js/components/new_post/new_post_contents.js
+++ frontend/public/js/components/new_post/new_post_contents.js
@@ -5,30 +5,32 @@ import { newPostCheckChange } from "../../action/user";
 export class NewPostContents extends Component {
     template() {
         const { user } = store.getState();
-        return `<div class="contents_add_img">
-                    <div class="add_img_box">
-                        <label for="imgs">
-                            <img src='assets/img/img_icon.png'/>
-                            <p>0/10</p>
-                        </label>
-                        <input type="file" name="imgs" id="imgs" multiple />
-                    </div>
+        return `<form action="/item" method="POST">
+            <div class="contents_add_img">
+                <div class="add_img_box">
+                    <label for="imgs">
+                        <img src='assets/img/img_icon.png'/>
+                        <p>0/10</p>
+                    </label>
+                    <input type="file" name="imgs" id="imgs" multiple />
                 </div>
-                <div class="contents_add_title">
-                    <input type="text" name="title" placeholder='글 제목'/>
-                </div>
-                <div class="contents_add_price">
-                    <input type="text" name="price" placeholder='$ 가격(선택사항)'/>
-                </div>
-                <div class="contents_add_word">
-                    <textarea name="word" wrap="virtual" placeholder='게시글 내용을 작성해주세요.'></textarea>
-                </div>
-                <div class="contents_add_locate">
-                    <img src="assets/img/locate_icon.png"/>
-                    <input type="text" name="locate" value="${
-                        user ? user.locate : ""
-                    }" readonly />
-                </div>`;
+            </div>
+            <div class="contents_add_title">
+                <input type="text" name="title" placeholder='글 제목'/>
+            </div>
+            <div class="contents_add_price">
+                <input type="text" name="price" placeholder='$ 가격(선택사항)'/>
+            </div>
+            <div class="contents_add_word">
+                <textarea name="word" wrap="virtual" placeholder='게시글 내용을 작성해주세요.'></textarea>
+            </div>
+            <div class="contents_add_locate">
+                <img src="assets/img/locate_icon.png"/>
+                <input type="text" name="locate" value="${
+                    user ? user.locate : ""
+                }" readonly />
+            </div>
+        </form>`;
     }
     initState() {
         return {
