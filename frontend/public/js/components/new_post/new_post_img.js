a983c15d202e3f55bb04c278ba6ef294dd9ca145
J017_김기한 <jum654@naver.com>
2021-10-07 16:58:49 +0900

    feat: 글쓰기 페이지 사진 업로드 기능 구현
    - input file element의 onchange 리스너에서 fileReader를 생성함.
    - fileReader에 업로드 될 때 발생하는 onload 리스너를 정의함.
    - onload 리스너에 내부 상태값 (imgs)을 추가해주는 로직 구성

---
+++ frontend/public/js/components/new_post/new_post_img.js
@@ -0,0 +1,39 @@
+import { Component } from "../../core/component";
+
+export class NewPostImg extends Component {
+    template() {
+        return `<div class="add_img_box">
+                    <label for="img">
+                        <img src='assets/img/img_icon.png'/>
+                        <p>${this.state.imgs.length}/10</p>
+                    </label>
+                    <input type="file" name="img" id="img" multiple />
+                </div>
+                ${this.state.imgs.reduce((pre, src) => {
+                    pre += `<div class="img_container">
+                        <img src="${src}">
+                        <button type="button">x</button>
+                    </div>`;
+                    return pre;
+                }, "")}`;
+    }
+    initState() {
+        return {
+            imgs: [],
+        };
+    }
+    setEvent() {
+        const $imgInput = this.$parent.querySelector("#img");
+        const pushImg = (changeEvent) => {
+            const reader = new FileReader();
+            const upload = (loadEvent) =>
+                this.setState({
+                    imgs: [...this.state.imgs, loadEvent.target.result],
+                });
+            reader.addEventListener("load", upload);
+            reader.readAsDataURL(changeEvent.target.files[0]);
+        };
+
+        $imgInput.addEventListener("change", pushImg);
+    }
+}
