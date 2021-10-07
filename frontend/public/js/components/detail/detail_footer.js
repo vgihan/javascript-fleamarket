5d0de51054bee6342a8f2307146d4a9640192f5b
J017_김기한 <jum654@naver.com>
2021-10-08 00:54:24 +0900

    feat: detail 페이지 footer layout 구성

--- frontend/public/js/components/detail/detail_footer.js
+++ frontend/public/js/components/detail/detail_footer.js
@@ -1,9 +1,24 @@
 import { Component } from "../../core/component";
-import { routing } from "../../router/router";
+import { store } from "../../store/store";
 
 export class DetailFooter extends Component {
     template() {
-        return ``;
+        const { like, price, seller } = this.props;
+        const { user } = store.getState();
+
+        return `<div class="like_price_box">
+            <div class="like_box">
+                <img src="assets/img/${like ? "wish_heart" : "heart"}.png">
+            </div>
+            <div class="price_box">
+                <p>${price.toLocaleString()}원</p>
+            </div>
+        </div>
+        <div class="chat_box">
+            <button type="button">${
+                user?.userId === seller ? "채팅 목록 보기" : "문의하기"
+            }</button>
+        </div>`;
     }
     initState() {
         return {
