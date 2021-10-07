69c585d1a34c4ca32af78e90fa3e2cbd2aab2e3f
J017_김기한 <jum654@naver.com>
2021-10-07 21:15:45 +0900

    feat: 글쓰기 페이지 카테고리 입력 버튼 추가 및 글쓰기 기능 구현
    - radio 버튼과 연결된 label의 색상을 변경시켜 클릭 시 선택됨을 보여줌.
    - title, category, price, contents, locate, user_uid를 필수 파라미터로
      하여 글을 게시한다.

--- backend/resources/item/index.js
+++ backend/resources/item/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const controller = require("./controller");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "./backend/images" });
+const upload = multer({ dest: "backend/uploads/" });
 
 router.get("", controller.get);
-router.post("", upload.array("img"), controller.post);
+router.post("", upload.single("imgs"), controller.post);
 router.put("", controller.put);
 router.delete("", controller.del);
 
