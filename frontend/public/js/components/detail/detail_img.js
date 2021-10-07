686ffa4f91bf0709321dcdb6c701893aaccadccf
J017_김기한 <jum654@naver.com>
2021-10-07 22:58:46 +0900

    feat: detail 페이지 컴포넌트 구현
    - contents, footeer, img, state로 각각 분리하여 구성함.

---
+++ frontend/public/js/components/detail/detail_img.js
@@ -0,0 +1,19 @@
+import { Component } from "../../core/component";
+import { routing } from "../../router/router";
+
+export class DetailImg extends Component {
+    template() {
+        return ``;
+    }
+    initState() {
+        return {
+            imgs: [],
+            num: { chat: null, heart: null, view: null },
+            seller: null,
+            locate: null,
+            like: false,
+            price: null,
+        };
+    }
+    async asyncUpdate() {}
+}
