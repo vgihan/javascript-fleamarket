52a9bb78211347e5d215552d94a5ba9b0a0f3e37
J017_김기한 <jum654@naver.com>
2021-10-08 00:25:03 +0900

    feat: detail 페이지 contents layout 구성
    - 상위 컴포넌트로 부터 전달받은 props 정보를 통해 layout을 구성함.

--- frontend/public/js/components/detail/detail_contents.js
+++ frontend/public/js/components/detail/detail_contents.js
@@ -1,23 +1,28 @@
 import { Component } from "../../core/component";
-import { routing } from "../../router/router";
 
 export class DetailContents extends Component {
     template() {
+        const { locate, num, seller, time, title, category, contents } =
+            this.props;
         return `<div class="title_box">
-            <p class="title"></p>
-            <p class="category_time"></p>
+            <p class="title">${title}</p>
+            <p class="category_time">${category} ∙ ${
+            time < 60 ? `${time}분 전` : `${parseInt(time / 60)}시간 전`
+        }</p>
         </div>
         <div class="contents_box">
-            <p class="contents"></p>
+            <p class="contents">${contents}</p>
         </div>
         <div class="num_box">
-            <p class="chat_like_view"></p>
+            <p class="chat_like_view">채팅 ${num.chat} ∙ 관심 ${
+            num.heart
+        } ∙ 조회 ${num.view}</p>
         </div>
         <div class="user_info_box">
-            <div class="intro"></div>
+            <div class="intro">판매자 정보</div>
             <div class="user_info">
-                <p class="user_id"></p>
-                <p class="locate"></p>
+                <p class="user_id">${seller}</p>
+                <p class="locate">${locate}</p>
             </div>
         </div>`;
     }
